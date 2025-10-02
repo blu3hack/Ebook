f9ec2be4745f4d3e1bba3326b3ca8cb14b1cbb1c
bayu jaya <ariesofyan55@gmail.com>
2025-10-02 11:25:02 +0700

    menambahkan Dashboard Controller dan component dashboard/index.jsx

---
+++ app/Http/Controllers/dashboard/DachboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DachboardController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Dashboard/Index');
+    }
+}
---
+++ resources/js/Pages/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+    return (
+        <div>
+            <span>Ini Adalah dashboard</span>
+        </div>
+    );
+}
+
+export default index;
